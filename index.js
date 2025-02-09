423898c5d232b1ba77b0a2734d20614ba35dae37
Ruben <ruben13478@gmail.com>
2025-02-08 23:11:23 -0600

    corrected Javascript

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    document.querySelectorAll('a').forEach(link => {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer'); // Security best practice
-    });
+document.querySelectorAll('section a').forEach(link => {
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener noreferrer'); // Security best practice
+});
